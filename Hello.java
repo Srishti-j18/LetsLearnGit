e21983425b979ae79b7ce4c4239bde29141fc198
Srishti-j18 <Srishti33164jaiswal@gmail.com>
2022-06-21 15:09:31 +0530

    intial commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello{
+
+    public static void main(String[] args){
+        String name="Srishti";
+        String surname="Jaiswal";
+
+        System.out.println(name + " "+surname +" :)");
+    }
+}
